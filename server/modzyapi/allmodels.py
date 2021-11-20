e44267a29f7807a017be5b01dabb29b7b03c9386
Srishti Bhargava <srishtibhargava@Srishtis-MacBook-Pro.local>
2021-11-20 10:56:58 -0800

    Added API to GET all models

--- server/modzyapi/allmodels.py
+++ server/modzyapi/allmodels.py
@@ -6,11 +6,10 @@ sys.path.insert(0, '..')
 from modzy import ApiClient
 
 dotenv.load_dotenv()
+BASE_URL = 'https://app.modzy.com/api'
+API_KEY = 'UekSgeQNOl22F8LtHCWu.KfnyQp94bIeYmrUEiHDM'
 
 def all_models():
-    BASE_URL = 'https://app.modzy.com/api'
-    API_KEY = 'UekSgeQNOl22F8LtHCWu.KfnyQp94bIeYmrUEiHDM'
-
     client = ApiClient(base_url=BASE_URL, api_key=API_KEY)
     params = {'is_active': True}
     models = client.models.get_models(**params)
