75f510ee25c21641ec1db6af6166049d1766ecbe
Srishti Bhargava <srishtibhargava@Srishtis-MacBook-Pro.local>
2021-11-20 10:54:42 -0800

    Added API to GET all models

--- server/modzyapi/allmodels.py
+++ server/modzyapi/allmodels.py
@@ -12,7 +12,7 @@ def all_models():
     API_KEY = 'UekSgeQNOl22F8LtHCWu.KfnyQp94bIeYmrUEiHDM'
 
     client = ApiClient(base_url=BASE_URL, api_key=API_KEY)
-
-    models = client.models.get_all()
+    params = {'is_active': True}
+    models = client.models.get_models(**params)
 
     return models 
