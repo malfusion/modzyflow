b2e0a05b639a5462f0039be803d73ea9cf2a47dc
malfusion <judenaveenraj@gmail.com>
2021-11-19 22:39:56 -0800

    Added comments

--- server/server.py
+++ server/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 
-
+# GET
 @app.route("/listmodels")
 def listmodels():
     return jsonify([{
@@ -14,6 +14,9 @@ def listmodels():
         "id": "someid" 
     }])
 
+
+# POST: ?flow=TestName
+# JSON will be sent in POST Body
 # Save a given workflow json to the filesystem, within "./workflows" directory
 @app.route("/saveflow")
 def saveflow():
@@ -21,11 +24,14 @@ def saveflow():
     return {"sample": flowname}
 
 
+# GET - URL Param: ?flow=TestName
 # Loads a given workflow json by name from the filesystem, from within "./workflows" directory
 @app.route("/loadflow")
 def loadflow():
     return "<p>Hello, World!</p>"
 
+
+# POST
 # Runs a given workflow
 @app.route("/runflow")
 def runflow():
