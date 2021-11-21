8f0cc3086dc843c33ee7f55e8b11ec58c3c6578e
Srishti Bhargava <srishtibhargava@Srishtis-MacBook-Pro.local>
2021-11-21 13:31:10 -0800

    Adding save and load workflow API

--- server/server.py
+++ server/server.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask
 from flask import jsonify
 from flask import request
@@ -5,6 +6,7 @@ from flask import request
 from modzyapi.allmodels import *
 app = Flask(__name__)
 
+workflows_dir = "./workflows"
 # GET
 @app.route("/api/listmodels")
 def listmodels():
@@ -14,9 +16,17 @@ def listmodels():
 # POST: ?flow=TestName
 # JSON will be sent in POST Body
 # Save a given workflow json to the filesystem, within "./workflows" directory
-@app.route("/api/saveflow")
+@app.route("/api/saveflow", methods=['POST'])
 def saveflow():
     flowname = request.args.get('flow')
+    flow = request.get_json()
+    json_object = json.dumps(flow)
+    print("Flowname " , flowname)
+    print ("Flow ", json_object)
+    if not os.path.isdir("./workflows"):
+        os.mkdir(workflows_dir)
+    with open(os.path.join(workflows_dir, flowname), "w") as outfile:
+        outfile.write(json_object)
     return {"sample": flowname}
 
 
@@ -24,8 +34,10 @@ def saveflow():
 # Loads a given workflow json by name from the filesystem, from within "./workflows" directory
 @app.route("/api/loadflow")
 def loadflow():
-    return "<p>Hello, World!</p>"
-
+    flowname = request.args.get('flow')
+    flowfile = open(workflows_dir + "/" + flowname)
+    flow = json.load(flowfile)
+    return jsonify(flow)
 
 # POST
 # Runs a given workflow
